089382e793f66decfd57f98776e6b88e833c11fe
Darya Shlyakhovaya <dshlyakhovaya1992@gmail.com>
2022-08-12 16:09:40 +0300

    add task2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,21 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const addElement = ingredients.map(element => {
+
+  const newIngredients = document.createElement('li');
+  newIngredients.textContent = element;
+  newIngredients.classList.add('item');
+  return newIngredients;
+  
+});
+const listIngredients = document.querySelector('#ingredients');
+listIngredients.append(...addElement);
+
+
+
+
+
+
