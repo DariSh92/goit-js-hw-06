7080b6ecc3f2d7f24b3aee4a2e5258b9bf783d1c
Darya Shlyakhovaya <dshlyakhovaya1992@gmail.com>
2022-08-15 13:43:49 +0300

    Add 6 task new

--- js/task-06.js
+++ js/task-06.js
@@ -5,14 +5,14 @@ const refs = {
 refs.input.addEventListener('blur', onInputBlur);
 
 function onInputBlur(event) {
-    if (event.currentTarget.value.length === +refs.input.dataset.length) {
+    if (event.currentTarget.value.length == refs.input.dataset['length']) {
         refs.input.classList.add('valid');
         return;
+       
     }
-    
+   
     refs.input.classList.add('invalid');
 };
-
 
 
 
