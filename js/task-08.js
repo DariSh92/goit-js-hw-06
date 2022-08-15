9339bc8382bff1df07a6b7c528694ee4bd4b873f
Darya Shlyakhovaya <dshlyakhovaya1992@gmail.com>
2022-08-15 16:32:50 +0300

    add 8 task

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
+const form = {
+    formEl: document.querySelector('.login-form'),
+    inputFormEmail: document.querySelector('input[type="email"]'),
+    inputFormPassword: document.querySelector('input[type="password"]'),
+    
+};
 
+form.formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    if (!form.inputFormEmail.value || !form.inputFormPassword.value) {
+        return alert('This inputs should be filled!');
+    }
+    else {
+        const formData = new FormData(event.currentTarget);
+        formData.forEach((value, name) => {
+            console.log('onFormSubmit -> name', name);
+            console.log('onFormSubmit -> value', value);
+            
+        });
+        form.formEl.reset();
+    }
+    
+}
