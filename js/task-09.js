2ea5d74e13fd21e1977f3f8107694a9360379d36
Darya Shlyakhovaya <dshlyakhovaya1992@gmail.com>
2022-08-15 17:07:38 +0300

    add 9 task

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+const refs = {
+  body: document.querySelector('body'),
+  spanEl: document.querySelector('.color'),
+  bntEl: document.querySelector('.change-color'),
 }
+
+refs.bntEl.addEventListener('click',changeBgColor);
+
+function changeBgColor() {
+  const BgColor = getRandomHexColor ();
+  refs.body.style.backgroundColor = BgColor;
+  refs.spanEl.textContent = BgColor;
+}
+
