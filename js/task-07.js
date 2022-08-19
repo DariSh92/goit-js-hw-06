de74886fde63717e0d711e56c5794f8acba7f0fb
Darya Shlyakhovaya <dshlyakhovaya1992@gmail.com>
2022-08-19 10:41:20 +0300

    fix task 7

--- js/task-07.js
+++ js/task-07.js
@@ -2,7 +2,7 @@ const refs = {
     input: document.querySelector('#font-size-control'),
     nameLabel: document.querySelector('#text'),
 };
-
+refs.nameLabel.style.fontSize = `${refs.input.value}px`;
 refs.input.addEventListener('input', onInputChange);
 
 function onInputChange(event) {
